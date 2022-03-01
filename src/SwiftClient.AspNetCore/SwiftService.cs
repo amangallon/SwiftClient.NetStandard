f1f6d58107b32577b4417448757fd77a58ce71e3
Antoine Mangallon <antoine.mangallon@ubisoft.com>
2022-03-01 15:54:55 -0500

    SwiftClient.AspNetCore : Expose options in swift service

--- src/SwiftClient.AspNetCore/SwiftService.cs
+++ src/SwiftClient.AspNetCore/SwiftService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
@@ -8,7 +9,7 @@ namespace SwiftClient.AspNetCore
 {
     public class SwiftService : Client, ISwiftService
     {
-        private readonly SwiftServiceOptions _options;
+        public SwiftServiceOptions Options { get; private set; }
 
         public SwiftService(IOptions<SwiftServiceOptions> options,
             ISwiftAuthManager authManager,
@@ -16,46 +17,46 @@ namespace SwiftClient.AspNetCore
             ISwiftLogger logger,
             string httpClientName = "swift") : base(authManager, logger)
         {
-            _options = options.Value;
-            SetRetryCount(_options.RetryCount);
-            SetHttpClient(httpClientFactory, httpClientName, _options.NoHttpDispose);
-            SetRetryPerEndpointCount(_options.RetryPerEndpointCount);
+            Options = options?.Value ?? throw new ArgumentNullException(nameof(options));
+            SetRetryCount(Options.RetryCount);
+            SetHttpClient(httpClientFactory, httpClientName, Options.NoHttpDispose);
+            SetRetryPerEndpointCount(Options.RetryPerEndpointCount);
         }
 
         public Task<SwiftResponse> DeleteDefaultContainerObjectsAsync(IEnumerable<string> objectIds)
-            => DeleteObjectsAsync(_options.DefaultContainer, objectIds);
+            => DeleteObjectsAsync(Options.DefaultContainer, objectIds);
 
         public Task<SwiftResponse> DeleteObjectAsync(string objectId, Dictionary<string, string> queryParams = null)
-            => DeleteObjectAsync(_options.DefaultContainer, objectId, queryParams);
+            => DeleteObjectAsync(Options.DefaultContainer, objectId, queryParams);
 
         public Task<SwiftResponse> DeleteObjectChunkAsync(string objectId, int segment)
-            => DeleteObjectChunkAsync(_options.DefaultContainer, objectId, segment);
+            => DeleteObjectChunkAsync(Options.DefaultContainer, objectId, segment);
 
         public Task<SwiftResponse> GetObjectAsync(string objectId, Dictionary<string, string> headers = null, Dictionary<string, string> queryParams = null)
-            => GetObjectAsync(_options.DefaultContainer, objectId, headers, queryParams);
+            => GetObjectAsync(Options.DefaultContainer, objectId, headers, queryParams);
 
         public Task<SwiftResponse> GetObjectRangeAsync(string objectId, long start, long end, Dictionary<string, string> headers = null, Dictionary<string, string> queryParams = null)
-            => GetObjectRangeAsync(_options.DefaultContainer, objectId, start, end, headers, queryParams);
+            => GetObjectRangeAsync(Options.DefaultContainer, objectId, start, end, headers, queryParams);
 
         public Task<SwiftResponse> HeadObjectAsync(string objectId, Dictionary<string, string> headers = null, Dictionary<string, string> queryParams = null)
-            => HeadObjectAsync(_options.DefaultContainer, objectId, headers, queryParams);
+            => HeadObjectAsync(Options.DefaultContainer, objectId, headers, queryParams);
 
         public Task<SwiftResponse> PostObjectAsync(string objectId, Dictionary<string, string> headers = null)
-            => PostObjectAsync(_options.DefaultContainer, objectId, headers);
+            => PostObjectAsync(Options.DefaultContainer, objectId, headers);
 
         public Task<SwiftResponse> PutManifestAsync(string objectId, Dictionary<string, string> headers = null, Dictionary<string, string> queryParams = null)
-            => PutManifestAsync(_options.DefaultContainer, objectId, headers, queryParams);
+            => PutManifestAsync(Options.DefaultContainer, objectId, headers, queryParams);
 
         public Task<SwiftResponse> PutObjectAsync(string objectId, Stream data, Dictionary<string, string> headers = null, Dictionary<string, string> queryParams = null)
-            => PutObjectAsync(_options.DefaultContainer, objectId, data, headers, queryParams);
+            => PutObjectAsync(Options.DefaultContainer, objectId, data, headers, queryParams);
 
         public Task<SwiftResponse> PutObjectAsync(string objectId, byte[] data, Dictionary<string, string> headers = null, Dictionary<string, string> queryParams = null)
-            => PutObjectAsync(_options.DefaultContainer, objectId, data, headers, queryParams);
+            => PutObjectAsync(Options.DefaultContainer, objectId, data, headers, queryParams);
 
         public Task<SwiftResponse> PutObjectChunkAsync(string objectId, byte[] data, int segment, Dictionary<string, string> headers = null, Dictionary<string, string> queryParams = null)
-            => PutObjectChunkAsync(_options.DefaultContainer, objectId, data, segment, headers, queryParams);
+            => PutObjectChunkAsync(Options.DefaultContainer, objectId, data, segment, headers, queryParams);
 
         public Task<SwiftResponse> PutPseudoDirectoryAsync(string objectId, Dictionary<string, string> headers = null, Dictionary<string, string> queryParams = null)
-            => PutPseudoDirectoryAsync(_options.DefaultContainer, objectId, headers, queryParams);
+            => PutPseudoDirectoryAsync(Options.DefaultContainer, objectId, headers, queryParams);
     }
 }
